3b85f0ba9fab8cfaca76b5278d11e7d3195d0f61
JuanNathaniel <>
2023-07-03 02:55:55 +0700

    Buat LandingPage

--- src/View/LandingPage.java
+++ src/View/LandingPage.java
@@ -5,12 +5,74 @@
  */
 package View;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.net.URL;
+import javax.swing.*;
+
 /**
  *
  * @author Juan Nathaniel
  */
-public class LandingPage {
-    public static void main(String[] args) {
-        System.out.println("test branch");
+public class LandingPage extends JFrame {
+
+    public LandingPage() {
+        this.setTitle("Landing Page");
+        
+        JPanel panel = new JPanel();
+        panel.setBounds(0, 0, 650, 950);
+        panel.setBackground(Color.gray);
+        
+//      profile
+        JButton profil = new JButton("Profile");
+        profil.setFont(new Font("Arial", Font.BOLD, 11));
+        profil.setBounds(20, 20, 70, 70);
+        panel.add(profil);
+        
+        //Text
+        JLabel selamatdatang = new JLabel("<html>Welcome to Kofi,<br>FRIENDS</html>");
+        selamatdatang.setFont(new Font("Arial", Font.BOLD, 23));
+        selamatdatang.setForeground(Color.white);
+        selamatdatang.setBounds(20, 120, 250, 70);
+        panel.add(selamatdatang);
+        
+        //Tagline
+        JLabel tagline = new JLabel("<html>Beragam warna di Kofi Coffee <br>siap menemani harimu!");
+        tagline.setFont(new Font("Arial", Font.PLAIN, 17));
+        tagline.setForeground(Color.white);
+        tagline.setBounds(20, 195, 250, 70);
+        panel.add(tagline);
+        
+        //Register
+        JButton register = new JButton("Register");
+        register.setFont(new Font("Arial", Font.BOLD, 16));
+        register.setBounds(100, 640, 400, 50);
+        panel.add(register);
+        
+        //Login
+        JButton login = new JButton("Login");
+        login.setFont(new Font("Arial", Font.BOLD, 16));
+        login.setBounds(100, 715, 400, 50);
+        panel.add(login);
+
+        //copyright
+        JLabel footer = new JLabel("                           Kofi By MJME             Whatsapp CS Kofi : 0817-1717-1717");
+        footer.setFont(new Font("Arial", Font.BOLD, 15));
+        footer.setForeground(Color.white);
+        footer.setBackground(Color.DARK_GRAY);
+        footer.setOpaque(true);
+        footer.setBounds(0, 820, 610, 90);
+        panel.add(footer);
+        //JFrame
+        this.add(panel);
+        panel.setLayout(null);
+        //
+        this.setSize(610, 950);
+        this.setLayout(null);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
     }
 }
